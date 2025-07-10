357195ba55c75f3c09612e367bba401413bc77fb
Alperen Can <alperencan2020@gmail.com>
2025-07-10 14:38:46 +0300

    update jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,19 @@
-def PROJECT_NAME = "JenkistTests-Unity"
-def CUSTOM_WORKSPACE = "A:\\Unity6"
-def UNITY_VERSION = "2022.3.62f1"
-def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION}\\Editor"
-
 pipeline {
     agent any
 
+    parameters {
+        booleanParam(name: 'BUILD_WINDOWS', defaultValue: true, description: 'Run build stage?')
+        booleanParam(name: 'DEPLOY_WINDOWS', defaultValue: true, description: 'Run deploy stage?')
+    }
+
     environment {
         PROJECT_PATH = "${CUSTOM_WORKSPACE}\\${PROJECT_NAME}"
-        BUILD_WINDOWS = 'true'       // Buraya true veya false yazabilirsin
-        DEPLOY_WINDOWS = 'true'      // Buraya true veya false yazabilirsin
     }
 
     stages {
         stage('Build Windows') {
             when {
-                expression { env.BUILD_WINDOWS == 'true' }
+                expression { params.BUILD_WINDOWS }
             }
             steps {
                 ws("${CUSTOM_WORKSPACE}\\${PROJECT_NAME}") {
@@ -32,7 +30,7 @@ pipeline {
 
         stage('Deploy Windows') {
             when {
-                expression { env.DEPLOY_WINDOWS == 'true' }
+                expression { params.DEPLOY_WINDOWS }
             }
             steps {
                 echo 'Deploy Windows stage is running...'
