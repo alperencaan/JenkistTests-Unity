799ff76e54bc4777240a2ac5e476730f3de0f513
Alperen Can <alperencan2020@gmail.com>
2025-07-10 15:06:27 +0300

    update jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,10 +12,12 @@ pipeline {
                 expression { env.BUILD_WINDOWS == 'true' }
             }
             steps {
-                script {
-                    bat """
-                    \"${UNITY_INSTALLATION}\\Unity.exe\" -quit -batchmode -projectPath \"${CUSTOM_WORKSPACE}\\${PROJECT_NAME}\" -executeMethod BuildScript.BuildWindows -logFile -
-                    """
+                ws("A:\\Unity6\\JenkistTests-Unity") {  // Burada çalışma dizinini değiştiriyoruz
+                    script {
+                        bat """
+                        "C:\\Program Files\\Unity\\Hub\\Editor\\2022.3.62f1\\Editor\\Unity.exe" -quit -batchmode -projectPath . -executeMethod BuildScript.BuildWindows -logFile -
+                        """
+                    }
                 }
             }
         }
