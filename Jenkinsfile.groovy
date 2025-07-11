cf87cfcf9f71cb1a418f19858679739bcd7dc4a8
Alperen Can <alperencan2020@gmail.com>
2025-07-11 09:20:40 +0300

    update jenksfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,11 +3,6 @@ def CUSTOM_WORKSPACE = "A:\\Unity6"
 def UNITY_VERSION = "2022.3.62f1"
 def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION}\\Editor"
 
-def PROJECT_NAME = "JenkistTests-Unity"
-def CUSTOM_WORKSPACE = "A:\\Unity6"
-def UNITY_VERSION = "2022.3.62f1"
-def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION}\\Editor"
-
 pipeline {
     agent any
 
