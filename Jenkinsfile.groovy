b2a2c6a8ae951e876ddb0f3e5161a2cc1854ef98
Alperen Can <alperencan2020@gmail.com>
2025-07-11 09:23:35 +0300

    update jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,11 +6,6 @@ def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION
 pipeline {
     agent any
 
-    environment {
-        PROJECT_PATH = "${CUSTOM_WORKSPACE}\\${PROJECT_NAME}"
-        UNITY_PATH = "${UNITY_INSTALLATION}\\Unity.exe"
-    }
-
     parameters {
         booleanParam(name: 'BUILD_WINDOWS', defaultValue: true, description: 'Build Windows?')
         booleanParam(name: 'DEPLOY_WINDOWS', defaultValue: false, description: 'Deploy Windows?')
@@ -19,10 +14,12 @@ pipeline {
     stages {
         stage('Check Env Variables') {
             steps {
-                echo "BUILD_WINDOWS = ${params.BUILD_WINDOWS}"
-                echo "DEPLOY_WINDOWS = ${params.DEPLOY_WINDOWS}"
-                echo "UNITY_PATH = ${env.UNITY_PATH}"
-                echo "PROJECT_PATH = ${env.PROJECT_PATH}"
+                script {
+                    echo "BUILD_WINDOWS = ${params.BUILD_WINDOWS}"
+                    echo "DEPLOY_WINDOWS = ${params.DEPLOY_WINDOWS}"
+                    echo "UNITY_PATH = ${UNITY_INSTALLATION}\\Unity.exe"
+                    echo "PROJECT_PATH = ${CUSTOM_WORKSPACE}\\${PROJECT_NAME}"
+                }
             }
         }
 
@@ -31,11 +28,10 @@ pipeline {
                 expression { return params.BUILD_WINDOWS }
             }
             steps {
-                // ❗ dir() kaldırıldı çünkü -projectPath zaten tüm path'i alıyor
                 bat """
-                "${env.UNITY_PATH}" -runTests -batchmode ^
-                -projectPath "${env.PROJECT_PATH}" ^
-                -testResults "C:\\temp\\results.xml" ^
+                "${UNITY_INSTALLATION}\\Unity.exe" -runTests -batchmode ^
+                -projectPath "${CUSTOM_WORKSPACE}\\${PROJECT_NAME}" ^
+                -testResults "C:\\\\temp\\\\results.xml" ^
                 -testPlatform editmode ^
                 -logFile -
                 """
